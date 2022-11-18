e59757d6d8c4be718098bd72a65eb6ebbaa58f64
JulienR2 <julien.rougeron@gmail.com>
2022-11-18 11:48:46 +0000

    add supabase auth

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,13 +4,13 @@ import express from 'express'
 
 import scriptsRouter from './scripts'
 import driveRouter from './drive'
+import { supabaseMiddleware } from './utils/supabase'
 
 const app = express()
 const port = process.env.PORT || 5000
 
-app.use('/api/scripts', scriptsRouter)
-app.use('/api/drive', driveRouter)
-
+app.use('/api/scripts', supabaseMiddleware, scriptsRouter)
+app.use('/api/drive', supabaseMiddleware, driveRouter)
 app.get('/api', (req, res) => {
   res.send('Hello World')
 })
