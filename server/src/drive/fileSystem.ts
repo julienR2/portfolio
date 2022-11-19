2519cef2a68a56572336ad807b5dd962c3914528
JulienR2 <julien.rougeron@gmail.com>
2022-11-19 19:41:40 +0000

    stream video and music

--- server/src/drive/fileSystem.ts
+++ server/src/drive/fileSystem.ts
@@ -6,22 +6,19 @@ import { File } from '../../../types'
 
 import { MEDIA_PATH } from '../constants'
 
-export const listDirectory = (req: Request): File[] | null => {
-  const currentUrl = req.protocol + '://' + req.get('host') + req.originalUrl
-  const rawQueryPath = req.path
-  const queryPath = path.join(MEDIA_PATH, rawQueryPath)
-
-  if (!fs.lstatSync(queryPath).isDirectory()) return null
-
+export const listDirectory = (
+  filePath: string,
+  currentUrl: string,
+): File[] | null => {
   const list: File[] = fs
-    .readdirSync(queryPath)
+    .readdirSync(filePath)
     .filter((name) => name[0] !== '.')
     .map((name) => ({
       id: name,
       name,
-      path: path.join(rawQueryPath, name),
+      path: filePath.replace(MEDIA_PATH, ''),
       url: new URL(`${currentUrl.replace(/\/$/, '')}/${name}`),
-      isDirectory: fs.lstatSync(path.join(queryPath, '/', name)).isDirectory(),
+      isDirectory: fs.lstatSync(path.join(filePath, '/', name)).isDirectory(),
     }))
 
   return list
--- server/src/drive/index.ts
+++ server/src/drive/index.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from 'express'
 import path from 'path'
 import fs from 'fs'
+import mime from 'mime-types'
+
 import { MEDIA_PATH } from '../constants'
 
 import { listDirectory } from './fileSystem'
@@ -8,19 +10,42 @@ import { listDirectory } from './fileSystem'
 const router = express.Router()
 
 router.get('/*', async (req: Request, res: Response) => {
-  const filePath = path.join(MEDIA_PATH, req.path)
+  const filePath = path.join(MEDIA_PATH, decodeURI(req.path))
 
   if (!fs.existsSync(filePath)) {
     return res.sendStatus(404)
   }
 
   if (fs.lstatSync(filePath).isDirectory()) {
-    const info = listDirectory(req)
+    const currentUrl = req.protocol + '://' + req.get('host') + req.originalUrl
+    const info = listDirectory(filePath, currentUrl)
 
     return res.json(info)
   }
 
-  res.sendFile(filePath)
+  const range = req.headers.range
+
+  if (!range) {
+    return res.sendFile(filePath)
+  }
+
+  const fileSize = fs.statSync(filePath).size
+  const CHUNK_SIZE = 10 ** 6
+  const [rawStart, rawEnd] = range.replace(/bytes=/, '').split('-')
+  const start = Number(rawStart)
+  const end = Math.min(Number(rawEnd) || start + CHUNK_SIZE, fileSize - 1)
+  const contentLength = end - start + 1
+  const headers = {
+    'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+    'Accept-Ranges': 'bytes',
+    'Content-Length': contentLength,
+    'Content-Type': mime.lookup(filePath) || 'text/plain',
+  }
+
+  res.writeHead(206, headers)
+
+  const stream = fs.createReadStream(filePath, { start, end })
+  stream.pipe(res)
 })
 
 export default router
