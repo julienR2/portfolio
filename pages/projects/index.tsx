4a4d12c6d31dada67d71819d94279964b3d51789
JulienR2 <julien.rougeron@gmail.com>
2023-05-29 13:06:11 +0100

    improve layout

---
+++ pages/projects/index.tsx
@@ -0,0 +1,11 @@
+import { Text } from '@mantine/core'
+
+import AppLayout from '@/layouts/AppLayout'
+
+export default function Projects() {
+  return (
+    <AppLayout>
+      <Text>Projects page</Text>
+    </AppLayout>
+  )
+}
