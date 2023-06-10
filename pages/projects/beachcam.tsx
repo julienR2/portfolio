040f12a96678467b901b6dd16667002d85686bae
julienr2 <julien.rougeron@gmail.com>
2023-06-11 00:39:17 +0100

    introduce usesupabse and useStore and display correctly cames

--- pages/projects/beachcam.tsx
+++ pages/projects/beachcam.tsx
@@ -4,86 +4,67 @@ import {
   SpotlightProvider,
   spotlight,
 } from '@mantine/spotlight'
-import { useSupabaseClient } from '@supabase/auth-helpers-react'
 import { IconSearch, IconVideo, IconX } from '@tabler/icons-react'
 import React from 'react'
 
 import VideoJS from '@/components/VideoJS'
 import AppLayout from '@/layouts/AppLayout'
-import { Database } from '@/types/supabase'
 import { DatabaseRow } from '@/types/utils'
+import { createStore, useStoreItem } from '@/utils/hooks/useStore'
+import { useSupabaseSelect } from '@/utils/hooks/useSupabase'
+
+const DEFAULT_CAM_IDS = [
+  'carcavelos',
+  'costacaparicasaojoao',
+  'bcsupertubosfixa',
+]
+
+const store = createStore('beachcam', {
+  selectedCams: null as DatabaseRow<'beach_cams'>[] | null,
+})
 
 export default function Projects() {
-  const [availableCams, setAvailableCams] = React.useState<
-    DatabaseRow<'beach_cams'>[]
-  >([])
-  const [selectedCams, setSelectedCams] = React.useState<
-    DatabaseRow<'beach_cams'>[]
-  >([])
-  const [defaultCams, setDefaultCams] = React.useState<
-    DatabaseRow<'beach_cams'>[]
-  >([])
+  const { data: availableCams } = useSupabaseSelect('beach_cams')
 
-  const supasbaseClient = useSupabaseClient<Database>()
+  const [selectedCams, setSelectedCams] = useStoreItem(store, 'selectedCams')
+
+  const defaultCams = React.useMemo(
+    () => availableCams?.filter(({ id }) => DEFAULT_CAM_IDS.includes(id)) || [],
+    [availableCams],
+  )
 
   const onSelectCam = React.useCallback(
     (cam: DatabaseRow<'beach_cams'>) => () => {
-      setSelectedCams((prevCams) => [
-        ...(!prevCams.length ? defaultCams : prevCams),
-        cam,
-      ])
+      setSelectedCams((prevCams) => [...(prevCams || defaultCams), cam])
     },
-    [defaultCams],
+    [defaultCams, setSelectedCams],
   )
 
   const actions = React.useMemo<SpotlightAction[]>(
     () =>
-      availableCams.map((cam) => ({
-        id: cam.id,
-        title: cam.name || '',
-        group: cam.location || '',
-        onTrigger: onSelectCam(cam),
-      })),
-    [availableCams, onSelectCam],
+      (availableCams || defaultCams || [])
+        .filter(
+          (cam) =>
+            !selectedCams?.find((selectedCam) => selectedCam.id === cam.id),
+        )
+        .map((cam) => ({
+          id: cam.id,
+          title: cam.name || '',
+          group: cam.location || '',
+          onTrigger: onSelectCam(cam),
+        })),
+    [availableCams, onSelectCam, selectedCams, defaultCams],
   )
 
-  React.useEffect(() => {
-    function getSelectedCams() {
-      const cams = window.localStorage.getItem('selected-cams')
-
-      if (!cams) return
-
-      setSelectedCams(JSON.parse(cams))
-    }
-
-    getSelectedCams()
-  }, [])
-
-  React.useEffect(() => {
-    async function getAvailableCams() {
-      const { data } = await supasbaseClient.from('beach_cams').select()
-
-      if (!data) return
-
-      setAvailableCams(data)
-
-      setDefaultCams(
-        data.filter(({ id }) =>
-          ['carcavelos', 'costacaparicasaojoao', 'bcsupertubosfixa'].includes(
-            id,
-          ),
+  const onDelete = React.useCallback(
+    (camId: string) => () =>
+      setSelectedCams((prevCams) =>
+        (prevCams?.length ? prevCams : defaultCams).filter(
+          ({ id }) => id !== camId,
         ),
-      )
-    }
-
-    getAvailableCams()
-  }, [supasbaseClient])
-
-  React.useEffect(() => {
-    if (!selectedCams.length) return
-
-    window.localStorage.setItem('selected-cams', JSON.stringify(selectedCams))
-  }, [selectedCams])
+      ),
+    [setSelectedCams, defaultCams],
+  )
 
   return (
     <AppLayout title="BeachCam">
@@ -103,14 +84,20 @@ export default function Projects() {
         </Group>
       </SpotlightProvider>
       <Grid w="100%" gutter="xl">
-        {(selectedCams.length ? selectedCams : defaultCams).map((cam) => (
-          <Grid.Col span={6} key={cam.id}>
-            <Group align="center" mb="sm" position="apart">
-              <Text weight={500}>{cam.name}</Text>
-              <ActionIcon radius="sm" size="sm" color="red">
-                <IconX size="1rem" color="gray" />
-              </ActionIcon>
-            </Group>
+        {(selectedCams || defaultCams).map((cam) => (
+          <Grid.Col xs={6} sm={6} key={cam.id}>
+            <Text weight={500} lineClamp={1}>
+              <Group align="center" mb="sm" position="apart">
+                {cam.name}
+                <ActionIcon
+                  radius="sm"
+                  size="sm"
+                  color="red"
+                  onClick={onDelete(cam.id)}>
+                  <IconX size="1rem" color="gray" />
+                </ActionIcon>
+              </Group>
+            </Text>
             <Card
               shadow="sm"
               padding="sm"
