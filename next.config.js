db5954127ba4d2b3b918a915a54462ff3d116045
julienr2 <julien.rougeron@gmail.com>
2023-06-27 15:58:27 +0100

    Revert "set back nextjs config"
    This reverts commit ba63148c73a7a37b85923048c3d8048d8d0cbacd.

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
-
-const { withPlausibleProxy } = require('next-plausible')
-
-module.exports = withPlausibleProxy({
-  customDomain: 'https://analytics.nowmad.io',
-})({
+const nextConfig = {
   reactStrictMode: true,
-})
+}
+
+module.exports = nextConfig
